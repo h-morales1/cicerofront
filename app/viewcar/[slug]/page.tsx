dc03b33d2daed30e8cc68b26ea02e0dc4af66fd8
Herbert M. <hmorales5@tutanota.com>
2024-03-30 14:41:29 -0500

    display retrieved car data

--- app/viewcar/[slug]/page.tsx
+++ app/viewcar/[slug]/page.tsx
@@ -1,3 +1,22 @@
-export default function ViewCar({ params }: { params: { slug: string } }) {
-  return <div>My car: {params.slug}</div>;
+export async function getCarData(id: string) {
+  const response = await fetch(`http://0.0.0.0:3000/cars/${id}`, {
+    method: "GET",
+  });
+  const carData = await response.json();
+  console.log(carData);
+  return carData;
+}
+
+export default async function ViewCar({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const carData = await getCarData(params.slug);
+  return (
+    <div>
+      Car stuff {carData.model}
+      <div>{carData.make}</div>
+    </div>
+  );
 }
