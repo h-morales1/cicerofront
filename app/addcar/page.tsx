72a3409515311150d7b7d2c2a844ef0f6d5f5e82
Herbert M. <hmorales5@tutanota.com>
2024-03-30 14:40:46 -0500

    properly redirect after adding a car

--- app/addcar/page.tsx
+++ app/addcar/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 import {
   Form,
@@ -15,87 +16,94 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  make: z.string().min(3).max(20),
-  model: z.string().min(3).max(20),
-  year: z.string().min(2).max(4),
-});
-
-export function CarForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      make: "",
-      model: "",
-      year: "",
-    },
+export default function AddCar() {
+  const router = useRouter();
+  const formSchema = z.object({
+    make: z.string().min(3).max(20),
+    model: z.string().min(3).max(20),
+    year: z.string().min(2).max(4),
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // do something with the form vals
-    console.log(values);
-    console.log(JSON.stringify(values));
-    fetch("0.0.0.0:3000/cars/", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
+  function CarForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        make: "",
+        model: "",
+        year: "",
       },
-      body: JSON.stringify(values),
     });
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+      // do something with the form vals
+      try {
+        const response = await fetch("http://0.0.0.0:3000/cars/", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
+        if (response.ok) {
+          const data = await response.json();
+          router.push(`/viewcar/${data.id}`);
+        }
+      } catch (error) {
+        router.push("/");
+      }
+    }
+
+    return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="make"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Make</FormLabel>
+                <FormControl>
+                  <Input placeholder="Porsche" {...field} />
+                </FormControl>
+                <FormDescription>Enter car's make.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="model"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Model</FormLabel>
+                <FormControl>
+                  <Input placeholder="911" {...field} />
+                </FormControl>
+                <FormDescription>Enter car's model.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="year"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Year</FormLabel>
+                <FormControl>
+                  <Input placeholder="1980" {...field} />
+                </FormControl>
+                <FormDescription>Enter car's manufacture year.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    );
   }
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="make"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Make</FormLabel>
-              <FormControl>
-                <Input placeholder="Porsche" {...field} />
-              </FormControl>
-              <FormDescription>Enter car's make.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="model"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Model</FormLabel>
-              <FormControl>
-                <Input placeholder="911" {...field} />
-              </FormControl>
-              <FormDescription>Enter car's model.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="year"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Year</FormLabel>
-              <FormControl>
-                <Input placeholder="1980" {...field} />
-              </FormControl>
-              <FormDescription>Enter car's manufacture year.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  );
-}
-
-export default function AddCar() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
