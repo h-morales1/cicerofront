2ef118e23d5bb0b3630420a0afb8c020eb6f6651
Herbert M. <hmorales5@tutanota.com>
2024-03-30 16:55:31 -0500

    added table of cars in index

--- app/page.tsx
+++ app/page.tsx
@@ -1,117 +1,31 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Car, columns } from "./columns";
+import { DataTable } from "./data-table";
 
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  carmake: z.string().min(3).max(20),
-  carmodel: z.string().min(3).max(20),
-  caryear: z.string().min(2).max(4),
-});
-
-export function CarForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      carmake: "",
-      carmodel: "",
-      caryear: "",
-    },
+async function onSubmit() {
+  // do something with the form vals
+  const response = await fetch("0.0.0.0:3000/cars/", {
+    method: "GET",
   });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // do something with the form vals
-    console.log(values);
-    console.log(JSON.stringify(values));
-    fetch("0.0.0.0:3000/cars/", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(values),
-    });
-  }
-
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="carmake"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Make</FormLabel>
-              <FormControl>
-                <Input placeholder="Porsche" {...field} />
-              </FormControl>
-              <FormDescription>Enter car's make.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="carmodel"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Model</FormLabel>
-              <FormControl>
-                <Input placeholder="911" {...field} />
-              </FormControl>
-              <FormDescription>Enter car's model.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="caryear"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Year</FormLabel>
-              <FormControl>
-                <Input placeholder="1980" {...field} />
-              </FormControl>
-              <FormDescription>Enter car's manufacture year.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  );
 }
 
-export async function getCars() {
-  const cars = await fetch("http://0.0.0.0:3000/cars").then((res) =>
-    res.json(),
+export async function getCars(): Promise<Car[]> {
+  const cars = await fetch("http://0.0.0.0:3000/cars", { method: "GET" }).then(
+    (res) => res.json(),
   );
 
   return cars;
 }
 
-export default function Home() {
-  const cardata = getCars();
+export default async function Home() {
+  const cardata = await getCars();
 
   console.log(cardata);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <CarForm />
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={cardata} />
       </div>
     </main>
   );
